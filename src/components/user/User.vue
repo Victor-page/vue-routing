7f491bf0c7af5ef7d093feabc5797ab3cda0a505
Victor Shevchuk <v.shevchuk@viseven.com>
2020-12-26 10:17:27 +0300

    first commit

---
+++ src/components/user/User.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>The User Page</h1>
+    <hr />
+    <button class="btn btn-primary" @click="navigateHome">Go Home</button>
+    <hr />
+    <transition name="slide" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateHome() {
+      this.$router.push({ name: "home" });
+    },
+  },
+};
+</script>
+
+<style>
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.6s;
+}
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+</style>
