b4bfd6dd0d1dcc148ee84d3fbcb87187d0f1e6de
Victor Shevchuk <v.shevchuk@viseven.com>
2020-12-26 11:26:28 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -28,11 +28,4 @@ new Vue({
   el: "#app",
   router,
   render: (h) => h(App),
-  created() {
-    if (sessionStorage.redirect) {
-      const redirect = sessionStorage.redirect;
-      delete sessionStorage.redirect;
-      this.$router.push(redirect);
-    }
-  },
 });
